b02c4b1d52ce499f37c7285b9edd6e66f2737d59
hieunh1989 <hieunh1989@gmail.com>
2013-08-06 11:20:53 +0700

    [#1]init webshop project width raisl-4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Webshop::Application.config.secret_key_base = '24884ffa5758ccca9814deb23e820cb0e702d728c6bb5f1916800b4a8ff3cb95437a4726537e73c974b2b6d6b75df5da30f45c38175b24a8048ac1fa49576e5d'
