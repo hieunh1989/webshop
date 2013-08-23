c62761b95cc6c0906e7ed95390b4114aa9dfd5ad
hieunh1989 <hieunh1989@gmail.com>
2013-08-23 16:36:38 +0700

    [#10]comple product to cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,23 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :query_cart
 
   def after_sign_in_path_for(user)
     if user.admin?
       return admin_users_path
     else
       return root_path
+    end
+  end
+
+
+  def query_cart
+    if cookies["order"].nil?
+     @count = 0
+   else
+      a = JSON.parse(cookies["order"])
+      @count = a["items"].map{ |i| i["quantity"] }.sum
     end
   end
 end
